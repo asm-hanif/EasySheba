7e7e11548f41d698ab8dc68dac4b7758f60e75de
Abu Sayed Muhammed Hanif <abusayed1952hanif@gmail.com>
2026-05-09 12:58:34 +0600

    Updated

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,18 +1,23 @@
 using System.Diagnostics;
+using EasySheba.Data;
 using EasySheba.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace EasySheba.Controllers
 {
     public class HomeController : Controller
     {
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(UserManager<IdentityUser> userManager)
+        public HomeController(UserManager<IdentityUser> userManager, ApplicationDbContext context)
         {
             _userManager = userManager;
+            _context = context;
         }
 
         // ========== HOME PAGE ==========
@@ -31,6 +36,17 @@ namespace EasySheba.Controllers
                 }
                 // Patient stays on Home page
             }
+
+            // Fetch real counts
+            var totalDoctors = await _context.Doctors.CountAsync();
+            var approvedHospitals = await _context.HospitalAdminProfiles.CountAsync(h => h.IsApproved);
+            var happyPatients = await _context.Appointments.Select(a => a.PatientId).Distinct().CountAsync();
+            if (happyPatients == 0) happyPatients = 5000; // fallback
+
+            ViewBag.DoctorCount = totalDoctors;
+            ViewBag.HospitalCount = approvedHospitals;
+            ViewBag.PatientCount = happyPatients;
+
             return View();
         }
 
